dbc7fdd0cfc5d6cb2979bd05a80e6d46f552e0bb
Niklas Fähnrich <niklasfahnrich@hotmail.com>
2022-06-16 16:01:23 +0200

    Update a comment in the dpendencyRedux store.ts

--- src/dependencyRedux/store.ts
+++ src/dependencyRedux/store.ts
@@ -13,6 +13,9 @@ export const store = configureStore({
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 
-// These hooks is just an implementation of the already existing hooks in redux, we have just given them the correct typing.
+/**
+ * These hooks is just an implementation of the already existing hooks in
+ * redux, we have just given them the correct typing.
+ */
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
