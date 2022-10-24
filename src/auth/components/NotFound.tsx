c4816e0ebeb8dfd1963881e52b1040baae03149f
Aladin El-Khalil <kontakt@intensivecode.se>
2022-10-24 17:02:27 +0200

    Removed auth and testing folders

--- src/auth/components/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-const NotFound = (): JSX.Element => {
-	const location = useLocation();
-
-	return <h1>No page found for {location.pathname}</h1>;
-};
-
-export default NotFound;
--- src/auth/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-const Home = (): JSX.Element => {
-	return <h1>This is the Homepage!</h1>;
-};
-
-export default Home;
